1f6d27d7376dfb61401cbfa94e49a94afdfbffa7
SpyderGersda <160939622+spydergersdaoff@users.noreply.github.com>
2026-06-19 21:28:20 +0200

    Refactor FILMS_DATA structure and remove duplicates

--- films.js
+++ films.js
@@ -1,11 +1,5 @@
 const FILMS_DATA = [
     {
-        title: "Scream 7",
-        embed: "https://vidzy.org/embed-8z17jkc8mfo0.html",
-        section: "tendances",
-        year: "2026"
-    },
-    {
         title: "Marsupilami",
         embed: "",
         section: "tendances",
