109fc7f70f34c9737cf921aff5e4015759169a11
SpyderGersda <160939622+spydergersdaoff@users.noreply.github.com>
2026-06-19 03:14:05 +0200

    Fix syntax for FILMS_DATA array

--- films.js
+++ films.js
@@ -12,69 +12,69 @@ const FILMS_DATA = [
         year: "2026"
     },    
     {
-        "title": "Scream",
-        "embed": "https://uqload.is/embed-cpnlk95lren0.html",
-        "section": "tendances",
-        "year": "1996"
+        title: "Scream",
+        embed: "https://uqload.is/embed-cpnlk95lren0.html",
+        section: "tendances",
+        year: "1996"
     },
     {
-        "title": "Scream 2",
-        "embed": "https://vidzy.org/embed-jkuxl865m3ul.html",
-        "section": "tendances",
-        "year": "1997"
+        title: "Scream 2",
+        embed: "https://vidzy.org/embed-jkuxl865m3ul.html",
+        section: "tendances",
+        year: "1997"
     },
     {
-        "title": "Scream 3",
-        "embed": "https://uqload.is/embed-ynwrrcmj83dv.html",
-        "section": "tendances",
-        "year": "2000"
+        title: "Scream 3",
+        embed: "https://uqload.is/embed-ynwrrcmj83dv.html",
+        section: "tendances",
+        year: "2000"
     },
     {
-        "title": "Scream 4",
-        "embed": "https://vidzy.live/embed-qvidkhl3cw6i.html",
-        "section": "tendances",
-        "year": "2011"
+        title: "Scream 4",
+        embed: "https://vidzy.live/embed-qvidkhl3cw6i.html",
+        section: "tendances",
+        year: "2011"
     },
     {
-        "title": "Scream 7",
-        "embed": "https://vidzy.org/embed-8z17jkc8mfo0.html",
-        "section": "tendances",
-        "year": "2026"
+        title: "Scream 7",
+        embed: "https://vidzy.org/embed-8z17jkc8mfo0.html",
+        section: "tendances",
+        year: "2026"
     },
     {
-        "title": "Scream Girl",
-        "embed": "https://uqload.is/embed-ljp1xrpw1ijh.html",
-        "section": "tendances",
-        "year": "2015"
+        title: "Scream Girl",
+        embed: "https://uqload.is/embed-ljp1xrpw1ijh.html",
+        section: "tendances",
+        year: "2015"
     },
     {
-        "title": "Scream Show",
-        "embed": "https://uqload.is/embed-1vd0ropfoty7.html",
-        "section": "tendances",
-        "year": "2009"
+        title: "Scream Show",
+        embed: "https://uqload.is/embed-1vd0ropfoty7.html",
+        section: "tendances",
+        year: "2009"
     },
     {
-        "title": "Scream Show 2",
-        "embed": "https://uqload.is/embed-p913x1f4kb6y.html",
-        "section": "tendances",
-        "year": "2011"
+        title: "Scream Show 2",
+        embed: "https://uqload.is/embed-p913x1f4kb6y.html",
+        section: "tendances",
+        year: "2011"
     },
     {
-        "title": "Scream VI",
-        "embed": "https://vidzy.live/embed-i06atg49d50c.html",
-        "section": "tendances",
-        "year": "2023"
+        title: "Scream VI",
+        embed: "https://vidzy.live/embed-i06atg49d50c.html",
+        section: "tendances",
+        year: "2023"
     },
     {
-        "title": "Screamboat",
-        "embed": "https://uqload.is/embed-kb5pz9cky4ok.html",
-        "section": "tendances",
-        "year": "2025"
+        title: "Screamboat",
+        embed: "https://uqload.is/embed-kb5pz9cky4ok.html",
+        section: "tendances",
+        year: "2025"
     },
     {
-        "title": "2001 Maniacs : Field of Screams",
-        "embed": "https://vidzy.live/embed-s5lch1p044zj.html",
-        "section": "tendances",
-        "year": "2010"
+        title: "2001 Maniacs : Field of Screams",
+        embed: "https://vidzy.live/embed-s5lch1p044zj.html",
+        section: "tendances",
+        year: "2010"
     }
 ];
