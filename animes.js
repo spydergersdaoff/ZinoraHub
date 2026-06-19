6f19cc2340cdd54f38f6776b280ef64c6fcb7727
SpyderGersda <160939622+spydergersdaoff@users.noreply.github.com>
2026-06-19 03:37:37 +0200

    Add new anime series 'Gagné ou Perdu' with episodes
    Added a new anime series 'Gagné ou Perdu' with detailed episodes.

--- animes.js
+++ animes.js
@@ -1,11 +1,69 @@
-const ANIMES_DATA = [
-    // Ajoute tes animés ici sur ce modèle :
-    /*
-    {
-        title: "Naruto",
-        embed: "https://vidzy.live/embed-exemple.html",
-        section: "a-ne-pas-manquer",
-        year: "2002"
+const ANIMES_DATA = [{
+        "title": "Gagné ou Perdu",
+        "section": "tendances",
+        "year": "2025",
+        "seasons": [
+            {
+                "seasonNumber": 1,
+                "episodes": [
+                    {
+                        "episodeNumber": 1,
+                        "title": "La fille du coach",
+                        "duration": "20min",
+                        "overview": "L’insécurité de Laurie concernant le championnat se manifeste sous la forme d’un blob sensible.",
+                        "embed": "https://vidzy.live/embed-nqj2rlxel30d.html"
+                    },
+                    {
+                        "episodeNumber": 2,
+                        "title": "L'arbitre",
+                        "duration": "20min",
+                        "overview": "L’insécurité de Laurie concernant le championnat se manifeste sous la forme d’un blob sensible.",
+                        "embed": "https://vidzy.live/embed-plgcpkerv2ps.html"
+                    },
+                    {
+                        "episodeNumber": 3,
+                        "title": "Les chaussures",
+                        "duration": "22min",
+                        "overview": "L’insécurité de Laurie concernant le championnat se manifeste sous la forme d’un blob sensible.",
+                        "embed": "https://vidzy.live/embed-wbgoqm36lci4.html"
+                    },
+                    {
+                        "episodeNumber": 4,
+                        "title": "L'inquiétude",
+                        "duration": "21min",
+                        "overview": "L’insécurité de Laurie concernant le championnat se manifeste sous la forme d’un blob sensible.",
+                        "embed": "https://vidzy.live/embed-286akrc0asgk.html"
+                    },
+                    {
+                        "episodeNumber": 5,
+                        "title": "Le vol",
+                        "duration": "19min",
+                        "overview": "L’insécurité de Laurie concernant le championnat se manifeste sous la forme d’un blob sensible.",
+                        "embed": "https://vidzy.live/embed-xijtwfu29tlx.html"
+                    },
+                    {
+                        "episodeNumber": 6,
+                        "title": "Mauvais signaux",
+                        "duration": "19min",
+                        "overview": "L’insécurité de Laurie concernant le championnat se manifeste sous la forme d’un blob sensible.",
+                        "embed": "https://vidzy.live/embed-cc9uxalrn4r1.html"
+                    },
+                    {
+                        "episodeNumber": 7,
+                        "title": "Je l'ai",
+                        "duration": "18min",
+                        "overview": "L’insécurité de Laurie concernant le championnat se manifeste sous la forme d’un blob sensible.",
+                        "embed": "https://vidzy.live/embed-wmsqj53bp7pa.html"
+                    },
+                    {
+                        "episodeNumber": 8,
+                        "title": "La finale",
+                        "duration": "28min",
+                        "overview": "L’insécurité de Laurie concernant le championnat se manifeste sous la forme d’un blob sensible.",
+                        "embed": "https://vidzy.live/embed-9zw7pxqoskf9.html"
+                    }
+                ]
+            }
+        ]
     }
-    */
 ];
